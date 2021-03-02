e0c1f06c81a4f917c1ca1a99e8aaa626e07e12a1
tawfiq <aharmim.tawfiq@gmail.com>
2021-03-02 14:46:57 +0100

    hhhh

---
+++ PORTFOLIO.js
@@ -0,0 +1,33 @@
+function p1(){
+    document.getElementById("page1O").style.display="grid";
+    document.getElementById("tanya").style.display="none";
+    document.getElementById("page3").style.display="none";
+    document.getElementById("page4").style.display="none";
+}
+function p2(){
+    document.getElementById("page1O").style.display="none";
+    document.getElementById("tanya").style.display="grid";
+    document.getElementById("page3").style.display="none";
+    document.getElementById("page4").style.display="none";
+}
+function p3(){
+    document.getElementById("page1O").style.display="none";
+    document.getElementById("tanya").style.display="none";
+    document.getElementById("page3").style.display="grid";
+    document.getElementById("page4").style.display="none";
+}
+function p4(){
+    document.getElementById("page1O").style.display="none";
+    document.getElementById("tanya").style.display="none";
+    document.getElementById("page3").style.display="none";
+    document.getElementById("page4").style.display="grid";
+}
+function send(){
+    document.getElementById("page1O").style.display="none";
+    document.getElementById("tanya").style.display="none";
+    document.getElementById("page3").style.display="grid";
+    document.getElementById("page4").style.display="none";
+}
+
+
+
